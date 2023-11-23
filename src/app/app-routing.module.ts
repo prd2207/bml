993558b07bc72e9ec61ed2d221000cadd6fcf5c7
prd2207 <mr.pradeep.rana22@gmail.com>
2023-11-23 17:17:23 +0530

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
+import { TermsandconditionComponent } from './termsandcondition/termsandcondition.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'app-home',component : HomeComponent},
+  {path : 'app-aboutus',component: AboutusComponent},
+  {path : 'app-privacypolicy',component : PrivacypolicyComponent},
+  {path: 'app-termsandcondition',component: TermsandconditionComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { PrivacypolicyComponent } from './privacypolicy/privacypolicy.component';
+import { TermsandconditionComponent } from './termsandcondition/termsandcondition.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    AboutusComponent,
+    PrivacypolicyComponent,
+    TermsandconditionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
